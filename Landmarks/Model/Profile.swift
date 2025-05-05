b3bf7f2c2d50ccc1ffeffb6c6ab7b96d8cda49a4
Arseni Kavalchuk <ars.kov@gmail.com>
2025-05-05 19:46:42 +0100

    Profile editor

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,30 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Arseni Kavalchuk on 5.05.25.
+//
+
+import Foundation
+
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "akavalchuk")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
